f03f72db20f0fdaf501106acf95c396300e0a66d
0draS0 <55214148+0draS0@users.noreply.github.com>
2019-11-27 00:15:11 -0600

    Agregado estructura princ. de arbol

--- Proyecto_Final/adivinador.c
+++ Proyecto_Final/adivinador.c
@@ -15,3 +15,47 @@ void Borrar (struct ArbolAdivinador *A)
 	free (A);
 }
 
+struct ArbolAdivinador *
+Cargar (void)  { FILE * fe;
+  struct ArbolAdivinador *A;
+   fe = fopen ("backup.pan", "r");
+  if (fe)
+    
+    {
+      A = LeeArbol (fe);
+      fclose (fe);
+    }
+  
+  else
+    
+    {
+      A = malloc (sizeof (struct ArbolAdivinador));
+      strcpy (A->Texto, "Aguila");
+      A->Si = NULL;
+      A->No = NULL;
+    } return (A);
+}
+
+  struct ArbolAdivinador *
+LeeArbol (FILE * fe)  { char Linea[80];
+  struct ArbolAdivinador *A;
+  fgets (Linea, 80, fe);
+  while (Linea[strlen (Linea) - 1] < ' ')
+    Linea[strlen (Linea) - 1] = 0;
+   A = malloc (sizeof (struct ArbolAdivinador));
+  strcpy (A->Texto, &Linea[1]);
+   if (Linea[0] == 'P')
+    
+    {
+      A->Si = LeeArbol (fe);
+      A->No = LeeArbol (fe);
+    }
+  
+  else
+    
+    {
+      A->Si = NULL;
+      A->No = NULL;
+    }
+   return (A);
+}
